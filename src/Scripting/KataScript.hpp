d92eac33a474db3851d023cec32e743e7b799f5b
brwhale <lazorpwnage@gmail.com>
2020-11-30 14:24:41 -0800

    allows indexing string literals

--- src/Scripting/KataScript.hpp
+++ src/Scripting/KataScript.hpp
@@ -912,7 +912,7 @@ namespace KataScript {
 					}
 				} else if (strings[i] == "[" || i + 2 < strings.size() && strings[i + 1] == "[") {
 					// list
-					bool indexOfIndex = i > 0 && strings[i - 1] == "]";
+					bool indexOfIndex = i > 0 && (strings[i - 1] == "]" || strings[i - 1].back() == '\"');
 					KSExpressionRef cur = nullptr;
 					if (!indexOfIndex && strings[i] == "[") {
 						// list literal
