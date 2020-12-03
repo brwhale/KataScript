bf14dde53a7f68e959c1609186d28481fc2d1011
brwhale <lazorpwnage@gmail.com>
2020-12-02 18:56:26 -0800

    js functions for interactive interpreter

--- jssrc/main.cpp
+++ jssrc/main.cpp
@@ -7,13 +7,22 @@
 KataScript::KataScriptInterpreter interp;
 
 EM_JS(void, print, (const char* s), {
-		  console.log('I received: ' + UTF8ToString(s));
-		  });
+	console.log('I received: ' + UTF8ToString(s));
+});
 
+void readLine(const char* s) {
+	interp.readLine(std::string(s));
+}
 
 int main() {
-//const char* a = getHtml("textarea");
-//interp.readLine(std::string(a));
-print(std::string("stringgggggggg").c_str());
+	interp.newFunction("print", [](KataScript::KSList args) {
+		std::string printstr = "";
+		for (auto&& arg : args) {
+			printstr += arg->getPrintString();
+		}
+		print(printstr.c_str());
+		return std::make_shared<KataScript::KSValue>();
+	});
+	print(std::string("stringgggggggg").c_str());
 }
 
