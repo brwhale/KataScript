c687dcc27ca78d0326d2421292af38a7b404894c
brwhale <lazorpwnage@gmail.com>
2020-12-02 19:27:38 -0800

    trying embind

--- jssrc/main.cpp
+++ jssrc/main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string>
 #include <emscripten.h>
+#include <emscripten/bind.h>
 #define KATASCRIPT_IMPL
 #include "../src/Scripting/KataScript.hpp"
 
@@ -11,8 +12,9 @@ EM_JS(void, print, (const char* s), {
 });
 
 EMSCRIPTEN_KEEPALIVE
-void readLine(const char* s) {
-	interp.readLine(std::string(s));
+void readLine(std::string s) {
+	print(s.c_str());
+	interp.readLine(s);
 }
 
 int main() {
@@ -24,6 +26,9 @@ int main() {
 		print(printstr.c_str());
 		return std::make_shared<KataScript::KSValue>();
 	});
-	print(std::string("stringaa").c_str());
+	print(std::string("stringa").c_str());
 }
 
+EMSCRIPTEN_BINDINGS(my_module) {
+	emscripten::function("readLine", &readLine);
+}
