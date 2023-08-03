ae8301672d61ad52bcada78a85a5676773b8c722
Garrett Skelton <lazor@GarrettWorkPC.localdomain>
2023-08-03 12:33:55 -0700

    new wasm

--- jssrc/build.sh
+++ jssrc/build.sh
@@ -1,1 +1,1 @@
-../../../emsdk/upstream/emscripten/emcc -std=c++2a --bind -s DISABLE_EXCEPTION_CATCHING=0 -O2 main.cpp -o kscript.js
+~/Coding/emsdk/upstream/emscripten/emcc -std=c++2a --bind -s DISABLE_EXCEPTION_CATCHING=0 -O2 main.cpp -o kscript.js
